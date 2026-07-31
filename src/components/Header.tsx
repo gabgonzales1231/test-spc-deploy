ae90a0cab4ed9681a27c9eccf56a1432eb0db91d
gabgonzales1231 <gabgonzales.ph@gmail.com>
2026-07-31 08:54:30 +0800

    remove epacd dropdown item

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,7 +42,7 @@ const navItems: NavItem[] = [
     hasDropdown: true,
     subItems: [
       { label: "Citizen's Charter", href: "/arta/citizens-charter" },
-      { label: "E-PACD", href: "/arta/epacd" },
+      // { label: "E-PACD", href: "/arta/epacd" },
       { label: "Redress Mechanism", href: "#redress", isRedress: true },
     ],
   },
